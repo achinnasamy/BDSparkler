9a5330c66ec578b651d5eb10941b3cb85b35c830
chinnasamy <chinnasamyad@gmail.com>
2017-10-09 10:47:28 +0530

    added code

---
+++ __main__.py
@@ -0,0 +1,8 @@
+from spark_job import sql_schema_job
+
+if __name__ == "__main__":
+
+    sql_schema_job.main()
+
+
+
--- spark_job/sql_schema_job.py
+++ spark_job/sql_schema_job.py
@@ -4,24 +4,25 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.types import IntegerType
 
-sparkconfig = SparkConf()
-sparkconfig.setMaster("local[*]")
-sparkconfig.setAppName("SparkCSVJOB")
 
 def print_each_line(eachLine):
     print eachLine
     return
 
+def main():
 
+    sparkconfig = SparkConf()
+    sparkconfig.setMaster("local[*]")
+    sparkconfig.setAppName("SparkCSVJOB")
 
-sparkcontext = SparkContext(conf= sparkconfig)
-sqlContext = SQLContext(sparkcontext)
+    sparkcontext = SparkContext(conf= sparkconfig)
+    sqlContext = SQLContext(sparkcontext)
 
-csvDF = sqlContext.read \
-                  .option("header", "true") \
-                  .csv("/home/dharshekthvel/Downloads/stop.csv")
+    csvDF = sqlContext.read \
+                      .option("header", "true") \
+                      .csv("/home/dharshekthvel/Downloads/stop.csv")
 
-csvDF.printSchema()
-# csvDF.show(2)
+    csvDF.printSchema()
+    csvDF.show(2)
 
 
